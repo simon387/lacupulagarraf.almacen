5c7288d9647b28c23e9a2e1db2c75192603cd2af
simone.celia <simone.celia@eng.it>
2022-10-21 00:43:23 +0200

    init

---
+++ deploy_localhost.bat
@@ -0,0 +1,2 @@
+rmdir C:\xampp\htdocs\lacupulagarraf.almacen /S /Q
+Xcopy /E /I ..\lacupulagarraf.almacen\ C:\xampp\htdocs\lacupulagarraf.almacen
---
+++ deploy_localhost.sh
@@ -0,0 +1,2 @@
+rm -rf /var/www/html/lacupulagarraf.almacen
+cp -R ../lacupulagarraf.almacen/ /var/www/html/
