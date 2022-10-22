0744594a6da41d273963c056588442cfca71328a
simone.celia <simone.celia@eng.it>
2022-10-22 11:30:05 +0200

    opts_imports2

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <?php
 require_once 'rest/config/protect.php';
 with('login.php', "scope");
+$page = "dashboard";
 ?>
 
 <?php include "head.php"; ?>
-
-<body id="page-top">
-
-<div id="wrapper">
-	<?php include "sidebar.php"; ?>
-	<div id="content-wrapper" class="d-flex flex-column">
-		<div id="content">
-			<?php include "topbar.php"; ?>
-			<?php include "dashboard.php"; ?>
-		</div>
-		<?php include "footer.php"; ?>
-	</div>
-</div>
+<?php include "wrapper.php"; ?>
 
 <?php include "logoutModal.php"; ?>
 <?php include "newPeriodModal.php"; ?>
-<?php $page = "dashboard"; ?>
 <?php include "jsImport.php"; ?>
