f2e03c0c5bd7f251fd7602f04ec11ab506c42984
simone.celia <simone.celia@eng.it>
2022-10-22 03:31:08 +0200

    spiana_tutt

--- components/footer.php
+++ components/footer.php
@@ -1,12 +1,13 @@
 <?php
-$f = @fopen("changelog.txt", 'r');
+require_once '../rest/config/config.php';
+$f = @fopen(SCRIPT_ROOT . "changelog.txt", 'r');
 $version = fgets($f);
 fclose($f);
 ?>
 <footer class="sticky-footer bg-white">
 	<div class="container my-auto">
 		<div class="copyright text-center my-auto">
-			<span>ALMACEN lacupulagarraf.com <?php echo $version ?> - Copyright &copy; <a target="_blank" href="https:\\www.simonecelia.it">www.simonecelia.it</a> 2022</span>
+			<span>ALMACEN lacupulagarraf.com <?php echo $version ?> - Copyright &copy; <a target="_blank" href="https://www.simonecelia.it">www.simonecelia.it</a> 2022</span>
 		</div>
 	</div>
 </footer>
--- components/pages/logs.php
+++ components/pages/logs.php
@@ -1,20 +1,21 @@
 <?php
 require_once '../../rest/config/protect.php';
 with('components/pages/login.php', "scope");
+require_once '../../rest/config/config.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
 <head>
 	<?php include "../head.title.php"; ?>
-	<link href="/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+	<link href="<?php echo SCRIPT_ROOT; ?>/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
 	<link
 		href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
 		rel="stylesheet">
-	<link href="/css/sb-admin-2.min.css" rel="stylesheet">
-	<link href="/css/spinner-overlay.css" rel="stylesheet">
-	<link href="/css/custom.css" rel="stylesheet">
-	<link href="/img/warehouse_122331.ico" rel="shortcut icon" />
+	<link href="<?php echo SCRIPT_ROOT; ?>/css/sb-admin-2.min.css" rel="stylesheet">
+	<link href="<?php echo SCRIPT_ROOT; ?>/css/spinner-overlay.css" rel="stylesheet">
+	<link href="<?php echo SCRIPT_ROOT; ?>/css/custom.css" rel="stylesheet">
+	<link href="<?php echo SCRIPT_ROOT; ?>/img/warehouse_122331.ico" rel="shortcut icon" />
 </head>
 
 <body id="page-top">
@@ -46,14 +47,14 @@ with('components/pages/login.php', "scope");
 </a>
 
 <?php include "../modals/logoutModal.php"; ?>
-<script src="/vendor/jquery/jquery.min.js"></script>
-<script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="/vendor/jquery-easing/jquery.easing.min.js"></script>
-<script src="/js/sb-admin-2.min.js"></script>
-<script src="/vendor/datatables/jquery.dataTables.min.js"></script>
-<script src="/vendor/datatables/dataTables.bootstrap4.min.js"></script>
-<script src="/js/custom.js"></script>
-<script src="/js/logs/logs.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/vendor/jquery/jquery.min.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/vendor/jquery-easing/jquery.easing.min.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/js/sb-admin-2.min.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/vendor/datatables/jquery.dataTables.min.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/js/custom.js"></script>
+<script src="<?php echo SCRIPT_ROOT; ?>/js/logs/logs.js"></script>
 
 <script type="application/javascript">
 	const userId = <?php echo $_SESSION['userid'] ?>;
